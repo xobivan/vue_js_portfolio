464b432a9c32d21f7249c53bb10d04933988c3af
xobivan <dvspivak98@gmail.com>
2025-02-10 16:57:27 +0000

    fixed deployed version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false, // Prevents eval() in production
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.devtool = false; // Disables eval() in Webpack
+    }
+  }
 })
